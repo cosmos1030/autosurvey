9a979d4c3d072ab16f7400fa0d1661b315c47b6f
cosmos1030 <dyk6208@gmail.com>
2023-03-25 14:46:03 +0000

    20230325

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
 import os
 import openai
 
-openai.api_key = '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'
-response = openai.Completion.create(model="text-davinci-003", prompt="could you give me some ideas for startup?", temperature=0, max_tokens=7)
+openai.api_key = 'sk-04UZQObJaNSmYaJRt$$$$$$$$$$$$$$$$$$$o3jV'
+prompt = input()
+response = openai.Completion.create(model="text-davinci-003", prompt=prompt, temperature=0, max_tokens=7)
 
 print(response)
+# print(response["choices"][0]["text"])
